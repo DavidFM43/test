eaadc708486132d4d82addee1bcc1835db1c9bed
David <dmorara@unal.edu.co>
2023-03-22 21:41:54 -0500

    Remove conda code

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import subprocess
 import whisper
 
 model = whisper.load_model("medium")
@@ -6,12 +5,3 @@ result = model.transcribe("/kaggle/input/mini-speech-diarization/dataset/test/te
 
 print(result["text"])
 
-
-# Construct the conda command to print package info
-conda_command = ["conda", "list"]
-
-# Use subprocess to run the conda command and capture the output
-output = subprocess.check_output(conda_command)
-
-# Decode the output from bytes to string and print it
-print(output.decode())
