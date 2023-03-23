b557c153824346784f1e15ffa5854dcd8a0ba235
David <dmorara@unal.edu.co>
2023-03-22 21:19:01 -0500

    Check conda packages

--- test.py
+++ test.py
@@ -1,5 +1,17 @@
+import subprocess
 import whisper
 
 model = whisper.load_model("medium")
 result = model.transcribe("/kaggle/input/mini-speech-diarization/dataset/test/test.wav")
+
 print(result["text"])
+
+
+# Construct the conda command to print package info
+conda_command = ["conda", "list"]
+
+# Use subprocess to run the conda command and capture the output
+output = subprocess.check_output(conda_command)
+
+# Decode the output from bytes to string and print it
+print(output.decode())
