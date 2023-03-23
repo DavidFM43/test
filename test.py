bd41d3a596bc75cd4a0e4fe058d2ee9f2d3ee3d0
David <dmorara@unal.edu.co>
2023-03-22 21:13:43 -0500

    more functionality

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 import whisper
 
-print("hola")
+model = whisper.load_model("medium")
+result = model.transcribe("/kaggle/input/mini-speech-diarization/dataset/test/test.wav")
+print(result["text"])
